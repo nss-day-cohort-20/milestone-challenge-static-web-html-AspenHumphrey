84456ee1eaba3ba41676ec104ca238f2a89dfe04
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-12 18:47:16 -0500

    adding github icon, adding color changes, trying to link js file with blog posts..

--- main.js
+++ main.js
@@ -3,25 +3,23 @@
 //Each item in the array must be an object that contains at a minimum 
 //the following information: Title, Copy, Date. 
 //Use JavaScript to add each post to the DOM.
+console.log("print" );
 
 
+function printBlogPost() {
 
+	var post = {};
 
+	blogPosts.title = ["Wonderland", "Zen"],
+	blogPosts.copy = [0, 1],
+	blogPosts.date = ["June", 06],
 
+	blogPosts.posts = function() {
+		console.log("", );
+		//post.innerHTML = `$`
+	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
