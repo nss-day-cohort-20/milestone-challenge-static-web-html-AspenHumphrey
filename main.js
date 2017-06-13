36a7e59f74f05e59a99e5a1e220ddb39a793bf24
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-13 09:21:22 -0500

    fidling with js file

--- main.js
+++ main.js
@@ -17,19 +17,19 @@ var posts = [
 		"author": "Aspen Humphrey",
 		"date": "June 12th",
 		"blog": "Unity between body and mind. Or for me letting go (of control). But what does all of this mean? What does it take to really be Zen? This has been the toughest challenge I have faced thus far. Not only here at NSS (while navigating Wonderland)- but in my daily life. I thoroughly enjoy knowing what I am doing, when, how, and why- always. I am truly searching for my center in every aspect of my life; being in Cohort 20 has escalated this idea and has been changing my core with such force I have whiplash! I am becoming more and more uncomfortable with each passing day. As this level grows,  I have no choice but to let go of more and more. Thus being said- I am having a difficult time letting go and trusting the method- not just in NSS but in my life. I am very thankful for this change and for this opportunity."
-	}
+	},
 ];
 
 function populateDom() {
 	for (var i = 0; i = posts.length; i++){
 		document.getElementById("blogposts").innerHTML += 
 
-		`
+		`<div>
 			<h2>${posts.title}</h2>
 		 	<p>${posts.blog}</p>
 		 	<h3>${posts.author}</h3>
 		 	<date>${posts.date}</date>
-		 `
+		 </div>`;
 	}
 
-}
+};
